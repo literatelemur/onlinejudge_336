eb511d37f42911331748b1b5bceb20ffd63b0c28
literatelemur <lukecarrington307@gmail.com>
2022-11-12 16:24:26 -0600

    Add files via upload

---
+++ onlinejudge_336.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class onlinejudge_336 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext()) {
+			int numEdges = input.nextInt();
+			int node1, int node2 = input.nextInt(), input.nextInt();
+			while (node1 != 0 and node2 != 0) {
+				
+			}
+		}
+	}
+}
